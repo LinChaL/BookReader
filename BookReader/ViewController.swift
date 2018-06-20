1e1f834a25435cd54d1beefa69693a45a9a1aa1c
LinChl <lcl3@meitu.com>
2018-06-20 11:09:57 +0800

    [feature]second commit

---
+++ BookReader/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BookReader
+//
+//  Created by linchl on 2018/6/20.
+//  Copyright © 2018年 linchl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let txtButton = UIButton(type: .custom)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
